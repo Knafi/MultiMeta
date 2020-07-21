73ec4ef85dd6ccf2665b06228b691b0798a548be
Knafi <68113115+Knafi@users.noreply.github.com>
2020-07-21 13:42:43 +0200

    Update install_MultiMeta.bat

--- install_MultiMeta.bat
+++ install_MultiMeta.bat
@@ -1,8 +1,13 @@
 :: MultiMeta Install Tool
-:: "Installs" MultiMeta shortcut on the Desktop and adds a Start menu entry.
-:: außerdem wird der uninstaller eine Ebene hoch geschoben (C:\sptvtools), damit er ordntlich funktioniert. Ansonsten kann es passieren, dass er den S-Meta Ordner nicht löscht
-mkdir "%appdata%\Microsoft\Windows\Start Menu\Programs\MultiMeta"
-copy "C:\sptvtools\S-Meta\S-Meta.lnk" "%appdata%\Microsoft\Windows\Start Menu\Programs\S-Meta"
-copy "C:\sptvtools\S-Meta\S-Meta.lnk" "%OneDrive%\Desktop"
-copy "C:\sptvtools\S-Meta\S-Meta.lnk" "%Userprofile%\Desktop"
-move /y "C:\sptvtools\S-Meta\uninstall_S-Meta.bat" "C:\sptvtools"
+:: Creates MultiMeta Directory, Desktop shortcut, Start menu entry
+
+set startmenu=%appdata%\Microsoft\Windows\Start Menu\Programs\MultiMeta
+set installdir=C:\MultiMeta\Tool\
+
+
+mkdir "%startmenu%"
+mkdir "%installdir%"
+move /y "%cd%\Tool" "%installdir%"
+copy "%installdir%\S-Meta.lnk" "%appdata%\Microsoft\Windows\Start Menu\Programs\S-Meta"
+copy "%installdir%\S-Meta.lnk" "%OneDrive%\Desktop"
+copy "%installdir%\S-Meta.lnk" "%Userprofile%\Desktop"
