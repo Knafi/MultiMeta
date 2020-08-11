e60699072ce75f0003a7e990691531efdb958939
Knafi <68113115+Knafi@users.noreply.github.com>
2020-08-11 15:41:08 +0200

    Update install_MultiMeta.bat

--- install_MultiMeta.bat
+++ install_MultiMeta.bat
@@ -1,25 +1,26 @@
+:: Keep it silent
+@ECHO OFF
+
 rem MultiMeta Install Tool
 rem Creates MultiMeta Directory, Desktop shortcut, Start menu entry
-rem Copies all Files to %installdir%
+rem Copies all files to %installdir%
 
-:: keep it silent
-@echo off
 
 :: Keep it variable for future changes
-:: If changed here, needs adapted accordingly in MultiMeta.bat and MultiMeta.odc
-set installdir="C:\MultiMeta"
+:: If changed here, needs to be adapted accordingly in MultiMeta.bat and MultiMeta.odc
+SET installdir="C:\MultiMeta"
 
 :: Create directory for start menu entry
-mkdir "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\MultiMeta"
+2>NUL MKDIR "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\MultiMeta"
 :: Create program/working directory
-mkdir "%installdir%\Tool\Work"
+2>NUL MKDIR "%installdir%\Tool\Work"
 :: Copy everything from current directory to program directory
-copy /y "%cd%" "%installdir%\Tool\"
+>NUL COPY /Y "%cd%" "%installdir%\Tool\"
 :: Move uninstall script one up to enable complete removal of MultiMeta's directories
-move /y "%installdir%\Tool\uninstall_MultiMeta.bat" "%installdir%"
+>NUL MOVE /Y "%installdir%\Tool\uninstall_MultiMeta.bat" "%installdir%"
 :: Copy shortcut to start menu directory
-copy "%installdir%\Tool\MultiMeta.lnk" "%appdata%\Microsoft\Windows\Start Menu\Programs\MultiMeta"
+>NUL COPY "%installdir%\Tool\MultiMeta.lnk" "%appdata%\Microsoft\Windows\Start Menu\Programs\MultiMeta"
 :: Copy shortcut to Desktop (Windows365 environment)
-copy "%installdir%\Tool\MultiMeta.lnk" "%OneDrive%\Desktop"
+>NUL COPY "%installdir%\Tool\MultiMeta.lnk" "%OneDrive%\Desktop"
 :: Copy shortcut to Desktop (local User)
-copy "%installdir%\Tool\MultiMeta.lnk" "%Userprofile%\Desktop"
+>NUL COPY "%installdir%\Tool\MultiMeta.lnk" "%Userprofile%\Desktop"
